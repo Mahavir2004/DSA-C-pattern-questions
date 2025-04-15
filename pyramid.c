62c61a612fab14d081f1d3b02b382b6c8602daa7
mahavir pandey <kmahavir906@gmail.com>
2025-04-16 05:24:54 +0530

    new prgrm

--- pyramid.c
+++ pyramid.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 
 int main(){
-int n,k;
+int n,num = 1;
+
 printf("Enter a number: ");
 scanf("%d", &n);
-printf("Enter the 2nd number: ");
-scanf("%d", &k);
+
 for (int i = 1; i <= n; i++)
 {
-    for (int j = 1; j <= k ; j++)
-    {
-        if (i==3|| j==2|| j==3|| j||4|| i==3)
-        {
-           printf("* ");
-        }
-         else{
-            printf("  ");
-         }
-         printf("\n");
-        
-    }
-    
+  for (int j = 1; j <= i; j++)
+  {
+   printf("%d ", num);
+   num++;
+   
+  }
+  printf("\n");
+  
 }
 
      return 0;
